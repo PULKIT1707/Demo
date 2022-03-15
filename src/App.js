a992ad390b42dc4a39db006684321f5647ac92e4
Pulkit Srivastava <32233945+PULKIT1707@users.noreply.github.com>
2022-03-15 11:21:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import HomePage from './Components/HomePage';
-import Login from './Components/Login';
-import { BrowserRouter, Switch ,Route, Router, Routes } from 'react-router-dom'
-import Error from './Components/Error';
+import Home from './Components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element= {<HomePage />}>
-          </Route>
-          <Route path="/login" element= {<Error />}>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </div>
   );
 }
